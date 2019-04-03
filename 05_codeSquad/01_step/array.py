44b60af9c896ecb73cdc4bbe596e272034ea241a
sel-dev <apricotsoul@gmail.com>
2019-04-03 16:59:03 +0900

    array.py added Reverse Array in HakerRank and codeSquad 1day practice.

---
+++ 05_codeSquad/01_step/array.py
@@ -0,0 +1,27 @@
+mport math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    reverse = []
+    for i in range(arr_count-1, -1, -1):
+        reverse.append(a[i])
+    return reverse
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
