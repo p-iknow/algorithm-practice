e91c19ee0574bbb0e2796e0553807e694011c719
sel-dev <apricotsoul@gmail.com>
2019-04-10 16:52:42 +0900

    Create - codesignal journey checkPalindrome.js

--- 04_codeSignal/journey/checkPalindrome.js
+++ 04_codeSignal/journey/checkPalindrome.js
@@ -1,7 +1,5 @@
-function reverseString(inputString){
-   retrun inputString.split("").reverse()
-} 
+// https://app.codesignal.com/arcade/intro/level-1/s5PbmwxfECC52PWyQ
 
 function checkPalindrome(inputString) {
-    
+   return inputString === inputString.split("").reverse().join("");
 }
