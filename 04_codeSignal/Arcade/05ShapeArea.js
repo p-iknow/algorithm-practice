734d72373369352c3f3e2faac4267c19f343ff21
P-iknow <apricotsoul@gmail.com>
2019-05-18 14:44:12 +0900

    doc: codeSignal-Arcade-isIPv4Address Solve

--- 04_codeSignal/Arcade/05ShapeArea.js
+++ 04_codeSignal/Arcade/05ShapeArea.js
@@ -2,18 +2,17 @@
 
 // my solution
 function shapeArea(n) {
-    let acc = 1;
-    if ( n === 1 ) {
-        return 1;    
-    }
-    for (let i=2; i< n+1; i++) {
-        acc = acc + ((i-1) * 4)
-    }
-    return acc
-
+  let acc = 1;
+  if (n === 1) {
+    return 1;
+  }
+  for (let i = 2; i < n + 1; i++) {
+    acc = acc + (i - 1) * 4;
+  }
+  return acc;
 }
 
-// Best 
+// Best
 function shapeArea(n) {
-    return Math.pow(n, 2) + Math.pow(n-1, 2);
+  return Math.pow(n, 2) + Math.pow(n - 1, 2);
 }
