096202e5742577f0252a3594aa8ee0cf81fa66c8
P-iknow <apricotsoul@gmail.com>
2019-10-08 23:05:45 +0900

    typo: 프로그래머스 netwokrk

--- Programmers/lv3/network.js
+++ Programmers/lv3/network.js
@@ -1,22 +1,33 @@
-const DFS = (i, computers, visited) => {
-  if (visited[i]) return;
-  visited[i] = true;
-  for (let j = 0; j < computers.length; j++) {
-    if (computers[i][j] === 1) {
-      DFS(j, computers, visited);
-    }
-  }
-};
-
 function solution(n, computers) {
-  const visited = new Array(n).fill(false);
   let answer = 0;
+  const isVisited = new Array(n).fill(false);
+
+  const DFS = function(computers, i) {
+    console.log('DFS excuted');
+    if (isVisited[i]) {
+      return;
+    }
+    isVisited[i] = true;
+    console.log(isVisited);
+
+    for (let j = 0; j < computers.length; j++) {
+      if (computers[i][j] === 1) {
+        console.log(`${i}, ${j}`);
+        console.log('connected');
+        DFS(computers, j);
+      }
+    }
+  };
+
   for (let i = 0; i < n; i++) {
-    if (visited[i] === false) {
+    if (!isVisited[i]) {
       answer++;
-      DFS(i, computers, visited);
+      console.log(isVisited, '도입부');
+      console.log(answer);
+      DFS(computers, i);
     }
   }
+
   return answer;
 }
 
