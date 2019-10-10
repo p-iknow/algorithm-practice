70b701885bce52fd761087ffb52b3ff1134b6565
P-iknow <apricotsoul@gmail.com>
2019-10-10 13:53:29 +0900

    solve: fastCampus toWiredCase

---
+++ fastCampus/toWeiredCase.js
@@ -0,0 +1,16 @@
+const toWiredCase = str => {
+  const wordArr = str.split(' ');
+  return wordArr
+    .map(word => {
+      return word
+        .split('')
+        .map((char, i) => {
+          if (i % 2 === 0) return char.toUpperCase();
+          return char.toLowerCase();
+        })
+        .join('');
+    })
+    .join(' ');
+};
+
+console.log(toWiredCase('aabb aabbd'));
