fa779f0d01407c30b1d69cc443f30174505531de
P-iknow <apricotsoul@gmail.com>
2020-08-30 14:57:50 +0900

    chore: lint

--- fastCampus/hideNumber.js
+++ fastCampus/hideNumber.js
@@ -1,9 +1,9 @@
 function hideNumbers(str) {
-  // 문자열을 뒤에서 4자리 잘라내기
-  // console.log(str.slice(-4));
-  // console.log(str.substring(str.length - 4));
+	// 문자열을 뒤에서 4자리 잘라내기
+	// console.log(str.slice(-4));
+	// console.log(str.substring(str.length - 4));
 
-  return '*'.repeat(str.length - 4) + str.slice(-4);
+	return '*'.repeat(str.length - 4) + str.slice(-4);
 }
 
 console.log(hideNumbers('01033334444')); // *******4444
